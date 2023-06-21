6a9e8c11ae4f04bd3c895f26d407dd0524a7479c
arka <arka8464@gmail.com>
2023-06-21 08:18:30 +0530

    added the columns

--- src/DataGrid.js
+++ src/DataGrid.js
@@ -1,38 +1,50 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
+// const columns = [
+//   { field: 'id', headerName: 'Sl No', width: 150 },
+//   {
+//     field: 'firstName',
+//     headerName: 'First name',
+//     width: 150,
+//     editable: true,
+//   },
+//   {
+//     field: 'lastName',
+//     headerName: 'Last name',
+//     width: 150,
+//     editable: true,
+//   },
+//   {
+//     field: 'age',
+//     headerName: 'Age',
+//     type: 'number',
+//     width: 110,
+//     editable: true,
+//   },
+//   {
+//     field: 'fullName',
+//     headerName: 'Full name',
+//     description: 'This column has a value getter and is not sortable.',
+//     sortable: false,
+//     width: 160,
+//     valueGetter: (params) =>
+//       `${params.getValue(params.id, 'firstName') || ''} ${
+//         params.getValue(params.id, 'lastName') || ''
+//       }`,
+//   },
+// ];
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, 'firstName') || ''} ${
-        params.getValue(params.id, 'lastName') || ''
-      }`,
-  },
+  // { field: 'checkbox', headerName: '', width: 100, renderCell: () => <input type="checkbox" /> },
+  { field: 'slno', headerName: 'Sl No', width: 100 },
+  { field: 'customer_order_id', headerName: 'CUSTOMER ORDER ID', width: 200 },
+  { field: 'salesorg', headerName: 'SALES ORG', width: 150 },
+  { field: 'distribution_channel', headerName: 'DISTRIBUTION CHANNEL', width: 100, wrapText: true },
+  { field: 'company_code', headerName: 'COMPANY CODE', width: 150 },
+  { field: 'order_creation_date', headerName: 'ORDER CREATION DATE', width: 200 },
+  { field: 'order_amount', headerName: 'ORDER AMOUNT', width: 150 },
+  { field: 'order_currency', headerName: 'ORDER CURRENCY', width: 150 },
+  { field: 'customer_number', headerName: 'CUSTOMER NUMBER', width: 200 },
 ];
 
 const rows = [
